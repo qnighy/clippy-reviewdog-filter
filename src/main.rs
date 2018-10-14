456f42530326192d37bacd7ab93ee8983d733f04
Masaki Hara <ackie.h.gmai@gmail.com>
2018-10-14 14:20:23 +0900

    Use actual stdin.

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,54 @@ extern crate xml;
 pub mod checkstyle;
 pub mod message;
 
-use std::io::{self, Cursor};
+use std::io;
 
 use xml::writer::EventWriter;
 
 use checkstyle::CheckstyleDoc;
 
 fn main() {
-    let stdin = Cursor::new(include_bytes!("sample.txt").to_vec());
+    let stdin = io::stdin();
+    let stdin = stdin.lock();
     let checkstyle = CheckstyleDoc::from_reader(stdin).expect("I/O error when reading input");
 
     let stdout = io::stdout();
     let stdout = stdout.lock();
     let mut xml = EventWriter::new(stdout);
     checkstyle.write_xml(&mut xml).expect("Error writing XML");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    use std::io::Cursor;
+
+    #[test]
+    fn test_from_reader() -> io::Result<()> {
+        let reader = Cursor::new(include_bytes!("sample.txt").to_vec());
+        let checkstyle = CheckstyleDoc::from_reader(reader)?;
+        assert_eq!(checkstyle.files.len(), 1);
+        let file = &checkstyle.files["src/main.rs"];
+        assert_eq!(file.errors.len(), 2);
+
+        let error0 = &file.errors[0];
+        assert_eq!(error0.column, 9);
+        assert_eq!(error0.line, 3);
+        assert_eq!(error0.message, "unused variable: `x`");
+        assert_eq!(error0.severity, "warning");
+        assert_eq!(error0.source, Some("unused_variables".to_owned()));
+
+        let error1 = &file.errors[1];
+        assert_eq!(error1.column, 13);
+        assert_eq!(error1.line, 3);
+        assert_eq!(
+            error1.message,
+            "casting u32 to f64 may become silently lossy if types change"
+        );
+        assert_eq!(error1.severity, "warning");
+        assert_eq!(error1.source, Some("cast_lossless".to_owned()));
+
+        Ok(())
+    }
 }
